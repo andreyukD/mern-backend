f756f34fb3424076a63e814423bac18832fb9846
Dima <wushu_dima@mail.ru>
2020-08-01 20:55:03 +0200

    added part of users api, validation

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
-const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/users-routes");
+
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route", 404);
